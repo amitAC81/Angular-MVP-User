de4588c7ff83a14db6fe72a5635b6c7cb3fbf4b8
Amit Chauhan <amit.chauhan@1rivet.local>
2020-02-17 17:56:43 +0530

    second commit

--- src/app/user/user-list-container/user-list-presentation/user-list-presentation.ts
+++ src/app/user/user-list-container/user-list-presentation/user-list-presentation.ts
@@ -9,9 +9,21 @@ import { UserListPresenter } from '../user-list-presenter/user-list-presenter';
 })
 export class UserListPresentation implements OnInit {
 
-  constructor() { }
+  // For hover Event
+  public hover: boolean;
+  constructor() {
+    this.hover = false;
+  }
 
   ngOnInit() {
+  }
+
+  // on hover to show edit and delete button
+  onHover() {
+    this.hover = true;
+  }
+  onHoverRemove(){
+    this.hover= false;
   }
 
 }
